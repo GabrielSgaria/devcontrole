dbb2109723deba28706a1e7987c535108f37e9af
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-09-03 17:33:43 -0300

    fetch: new customer

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -4,6 +4,21 @@ import { NextResponse } from 'next/server'
 import PrismaClient from "@/lib/prisma"
 
 
+export async function DELETE(request: Request){ 
+     
+    const session = await getServerSession(authOptions);
+    
+    if(!session || !session.user){
+        return NextResponse.json({ error: "Not authorized"}, {status: 401})
+    }
+
+    const { searchParams} = new URL(request.url);
+    const userId = searchParams.get("id");
+
+    return NextResponse.json({ok: true})
+}
+
+
 export async function POST(request: Request) {
     const session = await getServerSession(authOptions);
     if (!session || !session.user) {
--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,12 +1,32 @@
-export function CardCustomer() {
+'use client'
+import { api } from '@/lib/api'
+import { CustomerProps } from '@/utils/customers.type'
+
+
+export function CardCustomer({ customer }: { customer: CustomerProps }) {
+    async function handleDeleteCustomer() {
+        try {
+            const response = await api.delete("/api/customer", {
+                params: {
+                    id: customer.id
+                }
+            })
+            console.log(response.data)
+        }catch(err){
+            console.log(err)
+        }
+     }
+
     return (
         <article className="flex flex-col bg-gray-100/50 border-2 p-2 rounded-lg gap-2 hover:bg-gray-100 duration-300">
             <h2>
-                <a className="font-bold">Nome:</a> Mercado Silva
+                <a className="font-bold">Nome:</a> {customer.name}
             </h2>
-            <p><a className="font-bold">Email:</a> teste@teste.com</p>
-            <p><a className="font-bold">Telefone:</a> XX9999-9999</p>
-            <button className="bg-red-600 text-white px-4 rounded mt-2 self-start">
+            <p><a className="font-bold">Email:</a> {customer.email}</p>
+            <p><a className="font-bold">Telefone:</a> {customer.phone}</p>
+            <button
+                onClick={handleDeleteCustomer}
+                className="bg-red-600 text-white px-4 rounded mt-2 self-start">
                 Deletar
             </button>
         </article>
